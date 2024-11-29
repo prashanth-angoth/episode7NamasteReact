d7046816f387334f4b35d06b4be0fe9bbcf88684
unknown <angodhprashanth@gmail.com>
2024-11-29 17:17:42 +0530

    episode 7 notes routing and dynamically rendering the components

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+// if we want to create a component boiler plate code we just type RAFCE and enter,
+//  it'll create boilerplate code for you
+
+const About = ()=>{
+    return (<div><p>About </p>
+    <p>this is namste react course </p></div>)
+
+}
+
+export default About;
